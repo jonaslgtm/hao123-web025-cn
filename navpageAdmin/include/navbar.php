c0c7040f3b861901172cd5ff8726376377fbfec1
jonas <10245089@qq.com>
2025-05-22 10:10:13 +0800

    添加首页链接样式，并优化导航栏用户信息显示

--- navpageAdmin/include/navbar.php
+++ navpageAdmin/include/navbar.php
@@ -12,9 +12,9 @@ header("Pragma: no-cache");
 <!-- 顶部导航栏 -->
 <nav class="navbar navbar-light fixed-top flex-md-nowrap shadow">
     <a class="navbar-brand" href="dashboard.php">网站导航管理系统</a>    
-    <div class="navbar-user">
-        <a href="../index.html" class="btn btn-link" target="_blank"><i class="bi bi-house"></i> 访问首页</a>
-        <span class="user-name"><i class="bi bi-person-circle me-1"></i><?php echo htmlspecialchars($_SESSION['username']); ?></span>
+    <div class="navbar-user">        
+        <a href="../index.html" class="btn btn-link" target="_blank" title="在新标签页打开首页"><i class="bi bi-house"></i> 访问首页</a>
+        <span class="user-name">欢迎，<i class="bi bi-person-circle me-1"></i><?php echo htmlspecialchars($_SESSION['username']); ?></span>
         <a href="logout.php" class="btn btn-outline-secondary">退出 <i class="bi bi-box-arrow-right"></i></a>
     </div>
 </nav>
